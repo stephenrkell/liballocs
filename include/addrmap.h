baa9ed2a085bb251033559bd2102a3c56ddc75b7
Stephen Kell <srk31@srcf.ucam.org>
2012-03-15 20:12:17 +0000

    Major bugfixes to ELF symbol handling. Merge addrmap's memory_kind with process_image's. Use dladdr to get correct library load addresses on BSD. Other cleanups.

--- include/addrmap.h
+++ include/addrmap.h
@@ -2,7 +2,7 @@
 #define PMIRROR_ADDRMAP_H_
 
 /* x86_64 only, for now */
-#if !defined(__x86_64__) && !defined(X86_64)
+#if !defined(__x86_64__) && !defined(X86_64) && !defined(i386) && !defined(__i386__)
 #error Unsupported architecture.
 #endif
 
@@ -16,19 +16,28 @@ enum object_memory_kind
 	UNKNOWN,
 	STATIC,
 	STACK,
-	HEAP
+	HEAP,
+	ANON
 };
 	
 typedef enum object_memory_kind memory_kind;
 
+#if defined (X86_64) || (defined (__x86_64__))
 #define STACK_BEGIN 0x800000000000UL
+#else
+#define STACK_BEGIN 0xc0000000UL
+#endif
 
 /* HACK: on my system, shared libraries are always loaded at the top,
  * from 0x7eff00000000....
  * EXCEPT when we run ldd from a Makefile running dash, in which case
  * they show up at 0x2aaaa00000000+delta, which is weird. I should really
  * check the source of ld-linux.so, but for now, go with the lower addr. */
+#if defined (X86_64) || (defined (__x86_64__))
 #define SHARED_LIBRARY_MIN_ADDRESS 0x2aaa00000000UL
+#else
+#define SHARED_LIBRARY_MIN_ADDRESS 0xb0000000
+#endif
 inline enum object_memory_kind get_object_memory_kind(const void *obj)
 {
 	/* For x86-64, we do this in a rough-and-ready way. 
@@ -45,7 +54,11 @@ inline enum object_memory_kind get_object_memory_kind(const void *obj)
 	/* If the address is greater than RSP and less than top-of-stack,
 	 * it's stack. */
 	unsigned long current_sp;
+#if defined (X86_64) || (defined (__x86_64__))
 	__asm__("movq %%rsp, %0\n" : "=r"(current_sp));
+#else
+	__asm__("movl %%esp, %0\n" : "=r"(current_sp));
+#endif
 	if (__builtin_expect(addr >= current_sp && addr < STACK_BEGIN, 0)) return STACK;
 	
 	/* It's between HEAP and STATIC. HACK: use SHARED_LIBRARY_MIN_ADDRESS. */
