18d2dde8e6008a868d0b276a13a6267304170ab7
Stephen Kell <stephen.kell@oracle.com>
2014-01-08 17:05:34 +0000

    Add a vaguely working flow-insensitive sizeofness analysis in dumpallocs.ml, and a test case for it.

--- tools/lang/c/dumpallocs/dumpallocs.ml
+++ tools/lang/c/dumpallocs/dumpallocs.ml
@@ -50,11 +50,6 @@ module H = Hashtbl
 let abspath f =
    if String.get f 0 = '/' then f else (getcwd ()) ^ "/" ^ f
 
-(* Return the typesig of the type that e is calculating (a multiple of) the size of,
-   if any. 
-   We understand multiplications. 
-   We also want to detect buffer/string-size calculations, which do not use sizeof. 
-*)
 let rec sizeExprHasNoSizeof (e: exp) =
   match e with 
  | Const(c) -> true
@@ -73,17 +68,12 @@ let rec sizeExprHasNoSizeof (e: exp) =
  | StartOf((Var(v),o)) -> true
  | StartOf((Mem(ex),o)) -> sizeExprHasNoSizeof ex
 
-(* FIXME: split this into a "toplevel" that does the HasNoSizeof check,
-   and a recursive part which recurses *without* recursively doing the
-   HasNoSizeof check. *)
-let rec getSizeExpr (e: exp) =
-  if sizeExprHasNoSizeof e then Some(typeSig charType)
-  else begin
-  match e with
+
+let rec getSizeExpr (e: exp) (env : (int * typsig) list) = match e with
    |  BinOp(Mult, e1, e2, t) -> begin
-         match (getSizeExpr e1) with
+         match (getSizeExpr e1 env) with
            | Some(s1) -> Some(s1)
-           | None -> begin match (getSizeExpr e2) with
+           | None -> begin match (getSizeExpr e2 env) with
               | Some(s2) -> Some(s2)
               | None -> None
               end
@@ -91,8 +81,27 @@ let rec getSizeExpr (e: exp) =
    |  SizeOf(t) -> Some(typeSig t)
    |  SizeOfE(e) -> Some(typeSig (typeOf e))
    |  SizeOfStr(s) -> Some(typeSig charType)
+   |  Lval(lhost, offset) -> begin
+        match lhost with 
+           Var(v) -> begin
+             if v.vglob then None else try 
+               let found = Some(assoc v.vid env) in 
+               output_string Pervasives.stderr ("environment tells us that vid " ^ (string_of_int v.vid) ^ " has a sizeofness\n"); 
+               found 
+             with Not_found -> None
+           end
+        |  Mem(_) -> None
+      end
    | _ -> None
-  end
+   
+(* FIXME: split this into a "toplevel" that does the HasNoSizeof check,
+   and a recursive part which recurses *without* recursively doing the
+   HasNoSizeof check. *)
+let getSizeExprElseChar (e: exp) (env : (int * typsig) list) = 
+  let explicitSizeExpr = getSizeExpr e env in
+  match explicitSizeExpr with
+    None -> Some(typeSig charType)
+  | Some(t) -> Some(t)
 
 (*   |  SizeOf(t) -> Some(Pretty.sprint 80 (d_typsig () (typeSig t)))
    |  SizeOfE(e) -> Some(Pretty.sprint 80 (d_typsig () (typeSig (typeOf e))))
@@ -123,7 +132,7 @@ let rec warnIfLikelyAllocFn (i: instr) (f: varinfo) (arglist: exp list) =
          *)  end else (* None *)
       (output_string Pervasives.stderr ("call to function " ^ f.vname ^ " is not an allocation because of empty arglist\n"); (* None *) () )
 
-let rec extractUserAllocMatchingSignature i f arglist signature = 
+let rec extractUserAllocMatchingSignature i f arglist signature env = 
  (* destruct the signature string *)
  (output_string Pervasives.stderr ("Warning: matching against signature " ^ signature ^ "\n");
  let signatureFunction = 
@@ -140,21 +149,21 @@ let rec extractUserAllocMatchingSignature i f arglist signature =
        then Some((String.length (matched_string signatureArgSpec)) - 1 (* for the bracket*) - 1 (* because we want zero-based *))
        else (output_string Pervasives.stderr ("Warning: signature " ^ signature ^ " did not contain a capitalized arg spec element\n"); None)
  in match sizeArgPos with
-    Some(s) -> if (length arglist) > s then Some(f.vname, getSizeExpr (nth arglist s))
+    Some(s) -> if (length arglist) > s then Some(f.vname, getSizeExprElseChar (nth arglist s) env)
      else (output_string Pervasives.stderr ("Warning: signature " ^ signature 
      ^ " wrongly predicts allocation function " ^ f.vname ^ " will have at least " 
      ^ (string_of_int s) ^ " arguments, where here it has only " ^ (string_of_int (length arglist)) ^"\n"); None)
    | None -> None
  end)
 
-let rec getUserAllocExpr (i: instr) (f: varinfo) (arglist: exp list) = 
+let rec getUserAllocExpr (i: instr) (f: varinfo) (arglist: exp list) env = 
   let userVerdict = try begin
     let candidates = Str.split (regexp "[ \t]+") (Sys.getenv "LIBCRUNCH_ALLOC_FNS") in
     (* match f.vname with each candidate *) 
     let rec firstMatchingSignature cands =
       match cands with
         [] -> (* (output_string Pervasives.stderr ("Warning: exhausted candidate signatures in LIBCRUNCH_ALLOC_FNS matching function "  ^ f.vname ^ "\n"); *) None(* ) *)
-      | s::ss -> begin match extractUserAllocMatchingSignature i f arglist s with
+      | s::ss -> begin match extractUserAllocMatchingSignature i f arglist s env with
            None -> (* (output_string Pervasives.stderr ("Warning: signature " ^ s ^ " did not match function " ^ f.vname ^ "\n"); *) firstMatchingSignature ss (* ) *)
          | Some(s) -> Some(s)
         end
@@ -169,12 +178,12 @@ let rec getUserAllocExpr (i: instr) (f: varinfo) (arglist: exp list) =
    return Some(fn, optionalTypeSig)
    where fn is the function varinfo
    and optionalTypeSig is the type signature we inferred was being allocated, if we managed it *)
-let rec getAllocExpr (i: instr) (f: varinfo) (arglist: exp list) =
+let rec getAllocExpr (i: instr) (f: varinfo) (arglist: exp list) env =
   match f.vname with
-    | "malloc" -> Some (f.vname, getSizeExpr (nth arglist 0))
-    | "calloc" -> Some (f.vname, getSizeExpr (nth arglist 1))
-    | "realloc" -> Some (f.vname, getSizeExpr (nth arglist 1))
-    | _ -> getUserAllocExpr i f arglist 
+    | "malloc" -> Some (f.vname, getSizeExprElseChar (nth arglist 0) env)
+    | "calloc" -> Some (f.vname, getSizeExprElseChar (nth arglist 1) env)
+    | "realloc" -> Some (f.vname, getSizeExprElseChar (nth arglist 1) env)
+    | _ -> getUserAllocExpr i f arglist env
 
 (* HACK: copied from trumptr *)
 (* This effectively embodies our "default specification" for C code
@@ -229,12 +238,124 @@ let printAllocFn fileAndLine chan funvar allocExpr =
    in
    output_string chan (msg ^ allocExpr ^ "\n");
    flush chan
-              
+
+(* Currently our visitor visits each instruction, 
+   and for those that are calls, 
+   uses getAllocExpr to match those that are allocation function calls, 
+   extracting the sizeof subexpression from their argument list
+   in the process (getAllocExpr calls getSizeExpr). 
+   
+   PROBLEM: address-taken allocation functions?
+   We can perhaps conservatively overapproximate these. TODO.
+   
+   What we want it to do instead is:
+   - an intraprocedural analysis
+   - each SSA local is labelled with Maybe sizeExpr
+   - initially all labels are None
+   - ... except SSAs taking the value of a sizeof operator
+   - we then propagate (til a fixed point) as follows:
+   -- assignment propagates
+   -- multiplication propagates
+   -- addition should create/lookup a new type, but just output a warning for now
+   -- subtraction?
+   -- memory operations: warn if we write a sizeof to memory (see below).
+   
+   How do we interpret the CIL tree as SSA values?
+   -- recall: we have instructions, expressions and statements
+   -- expressions are trees, and have no effect
+   -- instructions have a single effect (and may contain one or more expressions)
+   -- statement have control flow [only].
+   
+   So we have to
+   -- visit whole functions, not instructions
+   -- recursively consider expressions
+   -- for a single instruction, update an environment of local variables
+   -- propagate this environment across control flow sequencing/branching
+   -- merge this environment across control flow joins
+   ... noting that any stmt may be a join; need to check its preds to see
+   
+   PROBLEM: helper functions which calculate a sizeof?
+   -- we can identify which functions *return* a sizeof
+   -- hmm, but what about writing a sizeof into a shared object?
+   -- okay -- we can identify which functions *output* a sizeof (i.e write it into non-local storage)
+
+ *)
+let rec untilFixedPoint f initialValue = begin
+  let printEl (vnum, ts) = "(" ^ string_of_int (vnum) ^ ", " ^ (Pretty.sprint 80 (d_typsig () ts)) ^ ") "
+  in 
+  let listAsString = "[" ^ (fold_left (^) "" (map printEl initialValue)) ^ "]"
+  in
+  output_string Pervasives.stderr ("hello from untilFixedPoint with initialValue " ^ listAsString ^ "\n"); 
+  let newValue = f initialValue in
+  if newValue = initialValue then initialValue else untilFixedPoint f newValue
+end
+
+let rec accumulateOverStatements acc (stmts: Cil.stmt list) =
+(* Our input is an environment mapping local variables to sizeofness, 
+   and a collection of statements. 
+   We propagate sizeofness from predecessor statements to successor statements, 
+   but how? 
+   We could take a completely flow-insensitive approach 
+   and simply ignore the pred/succ structure in stmts, 
+   instead just considering each instruction across all stmts. 
+   Let's try this. *)
+   let rec accumulateOverOneInstr acc i = output_string Pervasives.stderr "hello from accumulateOverOneInstr\n"; 
+       match i with
+         Call(_, f, args, l) -> acc
+       | Set((host, off), e, l) -> begin 
+           match host with
+            Var(v) -> if v.vglob then acc else begin
+               match getSizeExpr e acc with
+                 Some(t) -> (
+                 output_string Pervasives.stderr ("found some sizeofness in assignment to: " ^ (Pretty.sprint 80 (Pretty.dprintf  "\t%a\t" d_lval (host, off))) ^ " (vid " ^ (string_of_int v.vid) ^ ", sizeofTypsig " ^ (Pretty.sprint 80 (Pretty.dprintf "%a" d_typsig t)) ^  ")\n")
+                 ; (v.vid, t) :: (remove_assoc v.vid acc))
+              |  None -> acc
+            end
+          | Mem(e) -> acc
+         end 
+       | Asm(_, _, _, _, _, l) -> acc
+   in
+   let rec accumulateOverInstrs acc instrs = 
+     output_string Pervasives.stderr "hello from accumulateOverInstrs\n"; 
+     match instrs with 
+            [] -> acc
+    |  i :: is -> accumulateOverInstrs (accumulateOverOneInstr acc i) is
+   in
+   let rec accumulateOneStatement acc s = 
+      output_string Pervasives.stderr (Pretty.sprint 80 
+       (Pretty.dprintf  "Hello from accumulateOneStatement\n\t%a\t\n" d_stmt s)); 
+      match s.skind with
+        Instr(is: instr list) -> accumulateOverInstrs acc is
+   (*
+   |    Return(e : exp option, l : location) ->
+   |    Goto(s: stmt ref, l : location) ->
+   |    Break(l : location) ->
+   |    Continue (l : location) ->
+    *) 
+   |    Block(b) -> (* recurse over the block's stmts *) accumulateOverStatements acc b.bstmts
+   |    If (e, b1, b2, l) -> accumulateOverStatements (accumulateOverStatements acc b2.bstmts) b1.bstmts
+   |    Switch (e, b, ss, l) -> accumulateOverStatements (accumulateOverStatements acc ss) b.bstmts
+   |    Loop (b, l, continueLabel, breakLabel) -> accumulateOverStatements acc b.bstmts
+   |    TryFinally (tryBlock, finallyBlock, l) -> accumulateOverStatements (accumulateOverStatements acc tryBlock.bstmts) finallyBlock.bstmts 
+   |    TryExcept (tryBlock, _, exceptBlock, l)
+         -> accumulateOverStatements (accumulateOverStatements acc tryBlock.bstmts) exceptBlock.bstmts (* FIXME: instr list doesn't get handled*) 
+   | _ -> acc
+   in 
+   match stmts with 
+     [] -> acc
+  |  s :: ss -> accumulateOverStatements (accumulateOneStatement acc s) ss
+
+
+let rec propagateSizeEnv stmts env = accumulateOverStatements env stmts
+
 class dumpAllocsVisitor = fun (fl: Cil.file) -> object(self)
   inherit nopCilVisitor
   
   (* where we will write our alloc data *)
   val outChannel : out_channel option ref = ref None
+  
+  (* the mapping of local variables to their sizeofness *)
+  val sizeEnv : (int * typsig) list ref = ref []
   
   (* at construction time, open the output file *)
   initializer 
@@ -246,6 +367,14 @@ class dumpAllocsVisitor = fun (fl: Cil.file) -> object(self)
       end 
       with _ ->
         raise (Arg.Bad ("Cannot open file " ^ allocFileName))
+   
+
+  method vfunc (f: fundec) : fundec visitAction = 
+    Cil.prepareCFG(f);
+    Cil.computeCFGInfo f false; 
+    sizeEnv := untilFixedPoint (propagateSizeEnv f.sallstmts) []; 
+    (* now we know which locals are sizes, we can visit calls -- in vinstr *)
+    DoChildren
 
   method vinst (i: instr) : instr list visitAction = 
     ( output_string Pervasives.stderr ("considering instruction " ^ (
@@ -295,7 +424,7 @@ class dumpAllocsVisitor = fun (fl: Cil.file) -> object(self)
                          Sizeof T lets us terminate
                          Sizeof V also lets us terminate
                          Mul where an arg is a Sizeof lets us terminate *)
-                     match (getAllocExpr i v args) with
+                     match (getAllocExpr i v args !sizeEnv) with
                         Some(fn1, Some(ts)) -> 
                          printAllocFn fileAndLine chan v (stringFromSig ts); SkipChildren
                      |  Some(fn2, None) -> 
