ad8689a9ebb29bfedadaede6bfdfbbe5f5d1377d
Stephen Kell <srk31@cl.cam.ac.uk>
2014-02-28 15:48:07 +0000

    Don't save startup_brk unless requested at configure time.

--- src/addrmap.c
+++ src/addrmap.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include "addrmap.h"
 
-#ifdef USE_STARTUP_BRK
 intptr_t startup_brk;
 
 static void save_startup_brk(void) __attribute__((constructor(100))); /* top user priority... */
@@ -10,4 +9,3 @@ static void save_startup_brk(void)
 {
 	startup_brk = (intptr_t) sbrk(0);
 }
-#endif
