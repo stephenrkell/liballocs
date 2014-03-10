b5889600d06a5f18190f977d3de9c7be836eb077
Stephen Kell <srk31@cl.cam.ac.uk>
2014-03-10 14:50:18 +0000

    Change sloppy-dumptypes to work around ambiguity between long and long long ints.

--- tests/sloppy-dumptypes/sloppy-dumptypes.c
+++ tests/sloppy-dumptypes/sloppy-dumptypes.c
@@ -9,11 +9,11 @@ extern void __assert_fail (const char *__assertion, const char *__file,
 
 int main(void)
 {
-	/* We avoid using "signed long", and see if it's present 
-	 * in our types object, given that we *do* use unsigned long. */
+	/* We avoid using "signed short", and see if it's present 
+	 * in our types object, given that we *do* use unsigned short. */
 	__libcrunch_check_init();
-	unsigned long int slong_uniqtype = (unsigned long) dlsym(
-			0 /* RTLD_DEFAULT */, "__uniqtype__long_int");
-	if (!slong_uniqtype) __assert_fail("slong_uniqtype", __FILE__, __LINE__, __func__);
+	short unsigned int sshort_uniqtype_truncated = (short unsigned) (unsigned long) dlsym(
+			0 /* RTLD_DEFAULT */, "__uniqtype__short_int");
+	if (!sshort_uniqtype_truncated) __assert_fail("sshort_uniqtype_truncated", __FILE__, __LINE__, __func__);
 	return 0;
 }
