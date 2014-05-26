d96ec315e4138a4efcbf548dd403c8d305611c4c
Stephen Kell <srk31@cl.cam.ac.uk>
2014-05-26 15:38:40 +0100

    Add missing API calls to noop.c

--- src/noop.c
+++ src/noop.c
@@ -15,3 +15,26 @@ const void *__liballocs_typestr_to_uniqtype(const void *r)
 void __index_deep_alloc(void *ptr, int level, unsigned size_bytes) {}
 void __unindex_deep_alloc(void *ptr, int level) {}
 
+_Bool __liballocs_find_matching_subobject(signed target_offset_within_uniqtype,
+	void *cur_obj_uniqtype, void *test_uniqtype, 
+	void **last_attempted_uniqtype, signed *last_uniqtype_offset,
+		signed *p_cumulative_offset_searched)
+{
+	return 0; // failure
+}
+
+_Bool 
+__liballocs_get_alloc_info
+	(const void *obj, 
+	const void *test_uniqtype, 
+	const char **out_reason,
+	const void **out_reason_ptr,
+	void *out_memory_kind,
+	const void **out_object_start,
+	unsigned *out_block_element_count,
+	void **out_alloc_uniqtype, 
+	const void **out_alloc_site,
+	signed *out_target_offset_within_uniqtype)
+{
+	return 1; // abort
+}
