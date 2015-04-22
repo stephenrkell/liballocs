0eb0b01ffb5c6daf6278b943941d9b7500978359
Stephen Kell <srk31@cl.cam.ac.uk>
2015-04-22 16:17:22 +0100

    tests: add nopreload-alloca, and nopreload make rules.

---
+++ tests/nopreload-alloca/nopreload-alloca.c
@@ -0,0 +1,18 @@
+#define _GNU_SOURCE
+#include <alloca.h>
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <liballocs.h>
+
+extern __thread void *__current_allocsite __attribute__((weak));
+
+int main(void)
+{
+	printf("&__current_allocsite is %p\n", &__current_allocsite);
+	int *a = alloca(42 * sizeof (int));
+	a[41] = 0;
+	printf("Saw address %p holding %d\n", &a[41], a[41]);
+	return a[41];
+}
+
