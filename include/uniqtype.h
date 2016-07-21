f584ac1ff408a30572908eb99a286a7c26500745
Stephen Kell <srk31@cl.cam.ac.uk>
2016-07-21 23:05:26 +0100

    uniqtype.h: add MIT-style licence.

--- include/uniqtype.h
+++ include/uniqtype.h
@@ -1,3 +1,27 @@
+/*
+This is the license for uniqtype.h, a definition of run-time type
+information for compiled code.
+
+Copyright 2011--16, Stephen Kell <stephen.kell@cl.cam.ac.uk>
+
+Permission to use, copy, modify, and distribute this software and its
+documentation for any purpose and without fee is hereby granted,
+provided that the above copyright notice appear in all copies and that
+both the copyright notice and this permission notice and warranty
+disclaimer appear in supporting documentation, and that the name of
+the above copyright holders, or their entities, not be used in
+advertising or publicity pertaining to distribution of the software
+without specific, written prior permission.
+
+The above copyright holders disclaim all warranties with regard to
+this software, including all implied warranties of merchantability and
+fitness. In no event shall the above copyright holders be liable for
+any special, indirect or consequential damages or any damages
+whatsoever resulting from loss of use, data or profits, whether in an
+action of contract, negligence or other tortious action, arising out
+of or in connection with the use or performance of this software.
+*/
+
 #ifndef UNIQTYPE_H_
 #define UNIQTYPE_H_
 
