11d0ca2aba4d4cac5347e595127a9bd9ebb257bd
Stephen Kell <srk31@cl.cam.ac.uk>
2017-09-20 23:54:58 +0100

    tests/alloca: update to the new ARR0 heap handling. Also add assert about alloca allocator.

--- tests/alloca/alloca.c
+++ tests/alloca/alloca.c
@@ -9,18 +9,23 @@ extern __thread void *__current_allocsite __attribute__((weak));
 
 int main(void)
 {
-	void *a = alloca(42 * sizeof (int));
-	struct uniqtype *got_type = __liballocs_get_alloc_type(a);
+	void *o = alloca(42 * sizeof (int));
+	struct big_allocation *containing_bigalloc;
+	struct big_allocation *maybe_the_alloc;
+	struct allocator *a = __liballocs_leaf_allocator_for(o, &containing_bigalloc, &maybe_the_alloc);
+	assert(a == &__alloca_allocator);
+	struct uniqtype *got_type = __liballocs_get_alloc_type(o);
 	struct uniqtype *int_type = dlsym(RTLD_DEFAULT, "__uniqtype__int");
 	assert(int_type);
 	assert(got_type);
-	assert(got_type == int_type);
+	assert(UNIQTYPE_IS_ARRAY_TYPE(got_type));
+	assert(UNIQTYPE_ARRAY_ELEMENT_TYPE(got_type) == int_type);
 	
 	void *b = alloca(69105);
-	// can we still get the type of a?
-	struct uniqtype *got_type_again = __liballocs_get_alloc_type(a);
+	// can we still get the type of o (not b)?
+	struct uniqtype *got_type_again = __liballocs_get_alloc_type(o);
 	assert(got_type_again);
-	assert(got_type_again == int_type);
+	assert(got_type_again == got_type);
 	return 0;
 }
 
