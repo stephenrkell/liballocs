458000e948842e03cf5c578c75e3883cab402689
Stephen Kell <srk31@cl.cam.ac.uk>
2017-09-21 18:08:52 +0100

    tests/lib-test: add a comment to remind me what this test is doing.

--- tests/lib-test/lib-test.c
+++ tests/lib-test/lib-test.c
@@ -5,6 +5,9 @@
 
 int main(void)
 {
+	/* The liballocs source code includes some unit tests.
+	 * These are run as constructors from liballocs_test.so,
+	 * so dlopening that will run them. */
 	void *handle = dlopen("../../src/liballocs_test.so", RTLD_NOW);
 	assert(handle);
 	return 0;
