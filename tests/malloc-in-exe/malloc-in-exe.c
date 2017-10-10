5bc5311cbb371dc46a23ccbbebf604f1789e0c42
Stephen Kell <srk31@cl.cam.ac.uk>
2017-10-10 13:16:43 +0100

    tests: add malloc-in-exe.

---
+++ tests/malloc-in-exe/malloc-in-exe.c
@@ -0,0 +1,21 @@
+#define _GNU_SOURCE
+#include "liballocs.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct uniqtype;
+extern struct uniqtype __uniqtype__int;
+
+int main(void)
+{
+	printf("about to do user malloc()\n");
+	void *a = malloc(sizeof (int));
+	
+	struct uniqtype *t = __liballocs_get_alloc_type(a);
+	printf("type is %p\n", t);
+	// it's actually an array of 1 int
+	assert(UNIQTYPE_IS_ARRAY_TYPE(t)
+		&& UNIQTYPE_ARRAY_ELEMENT_TYPE(t) == &__uniqtype__int);
+	
+	return 0;
+}
