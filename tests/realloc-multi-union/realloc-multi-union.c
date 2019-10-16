ce0d7dc102eff41091f4e9a0088f5608bc883860
Stephen Kell <S.R.Kell@kent.ac.uk>
2019-10-16 17:41:38 +0100

    tests/realloc-multi-union: add explanatory comment to test source

--- tests/realloc-multi-union/realloc-multi-union.c
+++ tests/realloc-multi-union/realloc-multi-union.c
@@ -5,6 +5,23 @@
 
 extern struct uniqtype __uniqtype__short_unsigned_int;
 
+/* Here we test the __liballocs_add_type_to_block call and related helpers.
+ * This is a convenience for code like that in bzip2, which uses C's
+ * "effective type" rules' allowance for a type-changing update to memory.
+ * E.g. we can first use some memory as an int32_t array, and then re-use it
+ * as an array of twice as many int16_t simply by writing int16_t values
+ * into it.
+ *
+ * Code doing that defeats our tracking of the allocation type, so
+ * as a workaround, we provide a manual helper __liballocs_add_type_to_block
+ * (e.g. we patch bzip2 to make a call to this... sad times).
+ *
+ * This help replaces the malloc chunk's recorded type with a union
+ * of the old type and the given type. We go through the type-update
+ * manually to check that the set_type and get_or_create_union_type
+ * helpers work, and then go around again using the top-level
+ * add-type-to-block helper and check the outcome is the same. */
+
 int main(void)
 {
 	int *p = malloc(2 * sizeof(int));
