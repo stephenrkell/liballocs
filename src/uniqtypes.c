2adbc209d2827e83fb46f9903ff8bcd5e7fd8b7b
Stephen Kell <S.R.Kell@kent.ac.uk>
2019-10-16 19:20:22 +0100

    src/uniqtypes.c: shrink the set of types for which we use this hack

--- src/uniqtypes.c
+++ src/uniqtypes.c
@@ -4,6 +4,16 @@
 #include <assert.h>
 #include "liballocs.h"
 
+/* Why does this file exist?
+ * We never link it into a _preload.so because
+ * preloads can violate the uniqueness of global symbols.
+ * We do link it into clients who link -allocs, though.
+ * In short, it's a hacky way to create a bunch of uniqtypes
+ * that we don't dynamically generate. We probably should
+ * simply dynamically generate them all in liballocs.c. I've now
+ * removed the one that we dynamically generate. FIXME:
+ * dynamically generate the rest, then remove this file. */
+
 double __liballocs_blah;
 extern struct uniqtype __uniqtype____FUN_FROM___FUN_TO_unsigned_long_int;
 unsigned long __liballocs_uniqtypes_dummy() __attribute__((optimize("O0")));
@@ -11,21 +21,15 @@ unsigned long __liballocs_uniqtypes_dummy()
 {
 	/* NO! compiler can optimise past this even at -O0 */
 	// assert(0);
-	fprintf(stderr, "%p %p %p %p %p %p %p %p %p %p %p %p %p %p\n", 
-		&__uniqtype__void,
-		&__uniqtype__signed_char,
-		&__uniqtype__unsigned_char,
+	fprintf(stderr, "%p %p %p %p %p %p %p %p\n", 
 		&__uniqtype__int,
 		&__uniqtype__unsigned_int,
 		&/*__liballocs_uniqtype_of_typeless_functions*/ __uniqtype____FUN_FROM___FUN_TO_unsigned_long_int,
-		&__uniqtype__long_int,
-		&__uniqtype__unsigned_long_int,
 		&__uniqtype__short_int,
 		&__uniqtype__short_unsigned_int,
 		&__uniqtype__float,
 		&__uniqtype__double,
-		&__uniqtype____PTR_void,
-		&__uniqtype____PTR_signed_char
+		&__uniqtype____PTR_void
 	);
 	return 42ul;
 }
