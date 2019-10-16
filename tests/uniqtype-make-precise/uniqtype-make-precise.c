20251e8f4a963c32cb9980bba5b7377808a85508
Stephen Kell <S.R.Kell@kent.ac.uk>
2019-10-16 15:37:35 +0100

    tests/uniqtype-make-precise: repair rot now that __ARR0_ denotes a legit zero-length array and __ARR_ an imprecise-length one

--- tests/uniqtype-make-precise/uniqtype-make-precise.c
+++ tests/uniqtype-make-precise/uniqtype-make-precise.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include "liballocs.h"
 #include "uniqtype.h"
 
 struct xyzzy
@@ -6,34 +8,24 @@ struct xyzzy
 } x;
 extern struct uniqtype __uniqtype__xyzzy __attribute__((weak));
 
-/* imprecise uniqtype for "__ARR0_xyzzy" */
-const char *__uniqtype____ARR0_xyzzy_subobj_names[]  __attribute__((weak,
-section (".data.__uniqtype____ARR0_xyzzy, \"awG\", @progbits, __uniqtype____ARR0_xyzzy, comdat#")))= { (void*)0 };
-struct uniqtype __uniqtype____ARR0_xyzzy
-__attribute__((section (".data.__uniqtype____ARR0_xyzzy, \"awG\", @progbits, __uniqtype____ARR0_xyzzy, comdat#"))) = {
-	{ 0, 0, 0 },
-	UNIQTYPE_POS_MAXOFF_UNBOUNDED /* pos_maxoff */,
-	{ array: { 1, UNIQTYPE_ARRAY_LENGTH_UNBOUNDED } },
-	/* make_precise */ __liballocs_make_array_precise_with_memory_bounds, /* related */ {
-		{ { t: { &__uniqtype__xyzzy } } }
-	}
-};
-
 int main(void)
 {
-	/* Bare-bones test of the make-precise thing. 
+	/* Bare-bones test of the make-precise thing.
 	 * We ask the function to make a precise version of itself
 	 * for a 1-element array. */
-	struct uniqtype *precise = __uniqtype____ARR0_xyzzy.make_precise(
-		&__uniqtype____ARR0_xyzzy, NULL, 0,
+	struct xyzzy *z = malloc(sizeof (struct xyzzy));
+	struct uniqtype *imprecise = __liballocs_get_alloc_type(z);
+	assert(imprecise->make_precise);
+			
+	struct uniqtype *precise = imprecise->make_precise(
+		imprecise, NULL, 0,
 		&x, &x, sizeof x, NULL, NULL);
-	
 	printf("We got back: %s\n", UNIQTYPE_NAME(precise));
 	
 	/* Now call it again: we should get the same pointer, not another
 	 * copy of the uniqtype. */
-	struct uniqtype *precise2 = __uniqtype____ARR0_xyzzy.make_precise(
-		&__uniqtype____ARR0_xyzzy, NULL, 0,
+	struct uniqtype *precise2 = imprecise->make_precise(
+		imprecise, NULL, 0,
 		&x, &x, sizeof x, NULL, NULL);
 	
 	assert(precise == precise2);
