5e0b12106e20e69662c164b2002f7e913d7e3a63
Stephen Kell <S.R.Kell@kent.ac.uk>
2019-12-16 11:47:21 +0000

    tests: set a LIBALLOCS_BUILD envvar and use it in lib-test, now that we may have a separate build dir

--- tests/lib-test/lib-test.c
+++ tests/lib-test/lib-test.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
 int main(void)
@@ -8,7 +9,12 @@ int main(void)
 	/* The liballocs source code includes some unit tests.
 	 * These are run as constructors from liballocs_test.so,
 	 * so dlopening that will run them. */
-	void *handle = dlopen("../../src/liballocs_test.so", RTLD_NOW);
+	char *path = NULL;
+	assert(getenv("LIBALLOCS_BUILD"));
+	int ret = asprintf(&path, "%s/%s", getenv("LIBALLOCS_BUILD"), "/liballocs_test.so");
+	assert(ret > 0);
+	void *handle = dlopen(path, RTLD_NOW);
 	assert(handle);
+	free(path);
 	return 0;
 }
