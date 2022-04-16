3723b03d2945854b308c33151cd583717af8c861
Stephen Kell <srk31@srcf.ucam.org>
2022-04-16 17:56:54 +0100

    generic_malloc_index.h: fix calculation of bit index to clear (thanks Rahul Ramaka)

--- include/generic_malloc_index.h
+++ include/generic_malloc_index.h
@@ -408,8 +408,8 @@ static inline void __generic_malloc_index_delete(struct big_allocation *arena, v
 	/* The address *must* be in our tracked range. Assert this. */
 	assert(info->bitmap_base_addr == ROUND_DOWN_PTR(arena->begin, MALLOC_ALIGN*BITMAP_WORD_NBITS));
 	assert((uintptr_t) userptr >= (uintptr_t) info->bitmap_base_addr);
-	bitmap_clear_l(bitmap, ((uintptr_t) userptr - (uintptr_t) info->bitmap_base_addr) /
-			(MALLOC_ALIGN * BITMAP_WORD_NBITS));
+	bitmap_clear_l(bitmap, ((uintptr_t) userptr - (uintptr_t) info->bitmap_base_addr)
+			/ MALLOC_ALIGN);
 
 #ifdef TRACE_GENERIC_MALLOC_INDEX
 	fprintf(stderr, "*** Deleting entry for chunk %p, from bitmap at %p\n",
