70ea21b7f5ebd324f00802b5c0d1a20e23a5e966
Stephen Kell <srk31@srcf.ucam.org>
2023-12-02 10:34:32 +0000

    tests/anon-aliases: no longer expect like-definition like-typedef'd anonymous structs to be distinct, as they have the same associated name

--- tests/anon-aliases/anon-aliases.c
+++ tests/anon-aliases/anon-aliases.c
@@ -10,6 +10,13 @@ int main(void)
 	typedef long int long_int; // this used to cause us trouble
 	long_int i = 0l;
 
+	/* Each of x1, x2 and x3 is an object (it is declared as void, not pointer-to-void!)
+	 * in another compilation unit. We use liballocs to get each's type, and assert what
+	 * equalities we think should hold between them.
+	 * Since the introduction of "associated names" in libdwarfpp which cover
+	 * the case of 'typedef struct { ... } name;', we expect like-typedef'd like-definition
+	 * identical structures to have the same types. Previously the filename in which
+	 * the struct was declared would be factored into the type. */
 	assert(alloc_get_type(&x1));
 	assert(alloc_get_type(&x2));
 	assert(alloc_get_type(&x3));
@@ -22,8 +29,8 @@ int main(void)
 
 	// assert that x1 and x2 have same type (both 'header.h', different paths)
         assert(alloc_get_type(&x1) == alloc_get_type(&x2));
-	// assert that x1 and x2 have distinct type ('header.h' vs 'sameheader.h')
-        assert(alloc_get_type(&x2) != alloc_get_type(&x3));
+	// assert that x1 and x2 have same type ('header.h' vs 'sameheader.h' but we no longe care)
+        assert(alloc_get_type(&x2) == alloc_get_type(&x3));
 
 	return (int) i;
 }
