a3e1775f75fb0ca96c295dd1eac6d94c878fdeed
Stephen Kell <srk31@srcf.ucam.org>
2024-12-16 17:45:09 +1100

    workarounds.c: zero out the counts even if we didn't apply the workarounds

--- src/workarounds.c
+++ src/workarounds.c
@@ -42,51 +42,55 @@ void workaround_glibc_bugs(void)
 	if (!link_map_type)
 	{
 		debug_printf(0, "warning: no type information for ld.so link maps, so can't apply workarounds\n");
-		return;
+		goto out;
 	}
-
-	/* Do we have glibc? Quick check: libc.so.6 soname and
-	 * anything else? ideally symbol versions, but skip for now */
-	_Bool warned = 0;
-	_Bool seen_glibc = 0;
-	for (struct link_map *l = _r_debug.r_map; l; l = l->l_next)
+	else
 	{
-		/* Do we have type information for link map entries? */
-		struct allocator *a = alloc_get_allocator(l);
-		size_t sz = alloc_get_size(l);
-		const void *site = alloc_get_site(l);
-		struct uniqtype *u = alloc_get_type(l);
-		void *base = alloc_get_base(l);
-		assert(a);
 
-		if (l ==  _r_debug.r_map)
+		/* Do we have glibc? Quick check: libc.so.6 soname and
+		 * anything else? ideally symbol versions, but skip for now */
+		_Bool warned = 0;
+		_Bool seen_glibc = 0;
+		for (struct link_map *l = _r_debug.r_map; l; l = l->l_next)
 		{
-			debug_printf(0, "first link map is at %p, a %s-allocated object of size %u, type %s, "
-				"site %p, base %p\n",
-				l, a->name, (unsigned) sz, UNIQTYPE_NAME(u), site, base);
+			/* Do we have type information for link map entries? */
+			struct allocator *a = alloc_get_allocator(l);
+			size_t sz = alloc_get_size(l);
+			const void *site = alloc_get_site(l);
+			struct uniqtype *u = alloc_get_type(l);
+			void *base = alloc_get_base(l);
+			assert(a);
+
+			if (l ==  _r_debug.r_map)
+			{
+				debug_printf(0, "first link map is at %p, a %s-allocated object of size %u, type %s, "
+					"site %p, base %p\n",
+					l, a->name, (unsigned) sz, UNIQTYPE_NAME(u), site, base);
+			}
+
+			if (!site && a != &__static_symbol_allocator && !warned)
+			{
+				debug_printf(0, "warning: incomplete metadata for ld.so link maps\n");
+				warned = 1;
+			}
+
+			if (l->l_name && 0 == strcmp(basename(l->l_name), "libc.so.6"))
+			{
+				/* OK, seems to be glibc. FIXME: do more checks */
+				seen_glibc = 1;
+			}
 		}
-
-		if (!site && a != &__static_symbol_allocator && !warned)
+		if (!seen_glibc)
 		{
-			debug_printf(0, "warning: incomplete metadata for ld.so link maps\n");
-			warned = 1;
+			debug_printf(0, "not glibc so no workarounds applied\n");
+			goto out;
 		}
-
-		if (l->l_name && 0 == strcmp(basename(l->l_name), "libc.so.6"))
+		for (struct link_map *l = _r_debug.r_map; l; l = l->l_next)
 		{
-			/* OK, seems to be glibc. FIXME: do more checks */
-			seen_glibc = 1;
+			validate_or_fix_link_map(l, link_map_type);
 		}
 	}
-	if (!seen_glibc)
-	{
-		debug_printf(0, "not glibc so no workarounds applied\n");
-		return;
-	}
-	for (struct link_map *l = _r_debug.r_map; l; l = l->l_next)
-	{
-		validate_or_fix_link_map(l, link_map_type);
-	}
+out:
 	/* We may have done a bunch of liballocs queries in the above, and some
 	 * of them may have failed. So re-zero our counters... this is so that
 	 * we don't break test cases that pass based on zero failures.
