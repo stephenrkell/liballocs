0ea20eae303ec607fe7e51c3c0949669fd9aec69
Stephen Kell <srk31@srcf.ucam.org>
2025-07-25 18:18:28 +0100

    tests/alloca-clang: don't rely on a __uniqtype_int alias existing (e.g. if no libc metadata), and enforce DWARF 4

--- tests/alloca-clang/alloca-clang.c
+++ tests/alloca-clang/alloca-clang.c
@@ -19,15 +19,22 @@ const char *assertion, const char *file,
 ;
 #define assert(cond) \
   do { if (!(cond)) { \
-          __assert_fail("Assertion failed: " #cond "\n", __FILE__, __LINE__, __func__); \
+          __assert_fail(#cond , __FILE__, __LINE__, __func__); \
   } } while(0)
 extern __thread void *__current_allocsite __attribute__((weak));
+
+// HACK: pasted from liballocs.h which we're not yet able to include here
+#define BITS_STRING_CAT(tok1, tok2) tok1 ## tok2 /* extra indirection, working around how '##' inhibits expansion... */
+#define BITS_STRING(nbytes) BITS_STRING_CAT(BITS_STRING_, nbytes) /* ... i.e. it wouldn't work to just '##' in here */
+#define BITS_STRING_1 "8"
+#define BITS_STRING_2 "16"
+#define BITS_STRING_4 "32"
 
 int main(void)
 {
 	void *a = __builtin_alloca(42 * sizeof (int));
 	struct uniqtype *got_type = __liballocs_get_alloc_type(a);
-	struct uniqtype *int_type = dlsym(/*RTLD_NEXT*/ (void*)-1l, "__uniqtype__int");
+	struct uniqtype *int_type = dlsym(/*RTLD_NEXT*/ (void*)-1l, "__uniqtype__int$$" BITS_STRING(__SIZEOF_INT__));
 	assert(int_type);
 	assert(got_type);
 	assert(UNIQTYPE_IS_ARRAY_TYPE(got_type));
