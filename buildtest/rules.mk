accb698f6687ceb7b7a5db38cedf2bb11769a393
difcsi <zoltan.m@tuta.io>
2026-06-08 23:45:36 +0100

    feat: migrate container setup

--- buildtest/rules.mk
+++
@@ -1,6 +0,0 @@
-# the dockerfile template lives in the same dir as these makerules
-vpath Dockerfile.template $(dir $(lastword $(MAKEFILE_LIST)))
-
-Dockerfile: Dockerfile.template Dockerfile.env
-	cat $< | (. ./$(filter Dockerfile.env,$+) && \
-            envsubst '$$DISTRIBUTION $$PACKAGES $$MAKE_PARALLELISM' ) > $@ || (rm -f "$@"; false)
